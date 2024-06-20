a653b65485801e6538a8b083a81b9a8b0f529575
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-06-20 20:22:43 +0300

    build: add react query and react hook form with zod validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import styled from "styled-components";
+
 export default function Home() {
+	const Title = styled.h1`
+		font-size: 30px;
+		font-weight: bold;
+		color: red;
+	`;
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			home page
+			<Title>Title</Title>
 		</main>
 	);
 }
